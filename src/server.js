1de90e4d46390244c035a9166ced2bbd64e4e7c0
tkirksey <kirkseytc@gmail.com>
2026-04-16 12:17:24 -0400

    started base setup for api

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
+import express from 'express';
+import fs from 'fs';
+import yaml from 'js-yaml';
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+let specs;
+try {
+    specs = yaml.load(fs.readFileSync('./docs/openapi.yaml', 'utf-8'));
+} catch (error){
+    console.log('Failed to load OpenAPI specs: ', error);
+    process.exit(1);
+}
+
+app.get('/health', (req, res) => {
+    res.status(200).json({status: 'ok'});
+});
